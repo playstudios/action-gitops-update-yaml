c99b9f00559a7d5b1e4e2b6393e98d7ce392179f
Jon Walton <jonwalton@gmail.com>
2019-11-30 00:01:33 +0800

    initial implementation

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,52 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+// import * as process from 'process';
+// import * as cp from 'child_process';
+// import * as path from 'path';
 
-test('throws invalid number', async() => {
-    const input = parseInt('foo', 10);
-    await expect(wait(input)).rejects.toThrow('milleseconds not a number');
+import {setValue} from '../src/main';
+
+test('setValue', () => {
+    const tests = [
+        {
+            input: 'a:\n  b: hello',
+            paths: ['a.b'],
+            value: 'world',
+            output: 'a:\n  b: world'
+        },
+        {
+            input: 'a:\n  b: hello\nc:\n  d: abc',
+            paths: ['a.b', 'c.d'],
+            value: 'world',
+            output: 'a:\n  b: world\nc:\n  d: world'
+        },
+
+        // Currently unsupported. https://github.com/eemeli/yaml/issues/131
+        // {
+        //     input: 'a:\n  b: hello',
+        //     paths: ['a.b.c'],
+        //     value: 'world',
+        //     output: 'a:\n  b: world'
+        // }
+    ];
+
+    tests.forEach(test => {
+        expect(setValue(test.input, test.paths, test.value)).toMatch(test.output);
+    });
 });
 
-test('wait 500 ms', async() => {
-    const start = new Date();
-    await wait(500);
-    const end = new Date();
-    var delta = Math.abs(end.getTime() - start.getTime());
-    expect(delta).toBeGreaterThan(450);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-    process.env['INPUT_MILLISECONDS'] = '500';
-    const ip = path.join(__dirname, '..', 'lib', 'main.js');
-    const options: cp.ExecSyncOptions = {
-        env: process.env
-    };
-    console.log(cp.execSync(`node ${ip}`, options).toString());
-});
+// test('wait 500 ms', async() => {
+//     const start = new Date();
+//     await wait(500);
+//     const end = new Date();
+//     var delta = Math.abs(end.getTime() - start.getTime());
+//     expect(delta).toBeGreaterThan(450);
+// });
+//
+// // shows how the runner will run a javascript action with env / stdout protocol
+// test('test runs', () => {
+//     process.env['INPUT_MILLISECONDS'] = '500';
+//     const ip = path.join(__dirname, '..', 'lib', 'main.js');
+//     const options: cp.ExecSyncOptions = {
+//         env: process.env
+//     };
+//     console.log(cp.execSync(`node ${ip}`, options).toString());
+// });
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import yaml from 'yaml';
+import {promises as fs} from 'fs';
 
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+export function setValue(str: string, paths: string[], value: string): string {
+    let doc = yaml.parseDocument(str, {});
+    paths.forEach(path => {
+        str = doc.setIn(path.split('.'), value);
+    });
+    return yaml.stringify(doc);
 }
 
-run();
+export async function updateYaml(filePath: string, keys: string[], value: string): Promise<void> {
+    let buffer = await fs.readFile(filePath, {});
+    let result = setValue(buffer.toString("UTF-8"), keys, value);
+    await fs.writeFile(filePath, result, {encoding: "UTF-8"})
+}
+
+async function run() {
+    let filePath = core.getInput("file_path", {required: true});
+    let keys = core.getInput("keys", {required: true}).split(",");
+    let value = core.getInput("set_value", {required: true});
+    try {
+        await updateYaml(filePath, keys, value);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run().catch((e) => core.setFailed(e.message));
